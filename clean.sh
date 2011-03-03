aaa401929e07ec1ab3636fdced4e357644caed2f
Uwe Dauernheim <uwe@klarna.com>
2011-03-03 16:55:06 +0100

    Extend clean for OpenSSL

--- clean.sh
+++ clean.sh
@@ -4,3 +4,6 @@ rm -f EmbErl_*.tgz
 rm -f erl-xcomp-*.conf
 rm -rf otp_src_R14B01
 rm -f sysroot/openssl-*.tar.gz
+rm -f sysroot/usr/lib/bundle1.o
+rm -f sysroot/usr/lib/libSystem.dylib
+rm -f sysroot/usr/lib/libgcc_s.1.dylib
