68d9a0f1b11ddd5082cc61fef0dcd83ed1882ab4
Uwe Dauernheim <uwe@klarna.com>
2011-03-04 10:36:06 +0100

    Adopt openssl compilation for iOS 4.3

--- sysroot/build_openssl.sh
+++ sysroot/build_openssl.sh
@@ -17,10 +17,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
 
 VERSION="1.0.0d"
-SDKVERSION="4.2"
+SDKVERSION="4.3"
 
 CURRENTPATH=$(pwd)
 CRYPTOLIBPATH="${CURRENTPATH}/usr"
