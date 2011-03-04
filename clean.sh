69a42bee3242d35a8b15bb8bab1a6d90b652e034
Uwe Dauernheim <uwe@klarna.com>
2011-03-04 10:43:59 +0100

    Adopt clean script

--- clean.sh
+++ clean.sh
@@ -3,8 +3,4 @@
 rm -f ErliOS_*.tgz
 rm -rf otp_src_R14B01
 rm -f sysroot/openssl-*.tar.gz
-rm -f sysroot/usr/lib/libcrypto.a
-rm -f sysroot/usr/lib/libssl.a
-rm -f sysroot/usr/lib/bundle1.o
-rm -f sysroot/usr/lib/libSystem.dylib
-rm -f sysroot/usr/lib/libgcc_s.1.dylib
+rm -f sysroot/usr
